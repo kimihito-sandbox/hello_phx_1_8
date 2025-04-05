f413ba41d6f92a2b869596f5b91234754d18dc40
kimihito <tatsurotamashiro@gmail.com>
2025-04-05 16:29:50 +0900

    Guide: Components

--- lib/hello_phx_1_8_web/controllers/hello_html.ex
+++ lib/hello_phx_1_8_web/controllers/hello_html.ex
@@ -2,4 +2,12 @@ defmodule HelloPhx18Web.HelloHTML do
   use HelloPhx18Web, :html
 
   embed_templates "hello_html/*"
+
+  attr :messenger, :string, default: nil
+
+  def greet(assigns) do
+    ~H"""
+    <h2>Hello World, from {@messenger}!</h2>
+    """
+  end
 end
