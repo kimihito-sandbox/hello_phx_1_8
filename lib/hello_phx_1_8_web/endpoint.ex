e4a3e797353d5add24aa2e3262e5937326ea0a64
kimihito <tatsurotamashiro@gmail.com>
2025-04-05 15:11:04 +0900

    Guide: Function plugs

--- lib/hello_phx_1_8_web/endpoint.ex
+++ lib/hello_phx_1_8_web/endpoint.ex
@@ -50,5 +50,16 @@ defmodule HelloPhx18Web.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug :introspect
   plug HelloPhx18Web.Router
+
+
+  def introspect(conn, _opts) do
+    IO.puts """
+    Verb: #{inspect(conn.method)}
+    Host: #{inspect(conn.host)}
+    Headers: #{inspect(conn.req_headers)}
+    """
+    conn
+  end
 end
